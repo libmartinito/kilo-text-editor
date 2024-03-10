044db109d4bcde3b74838e1449bdf2e990d59226
libmartinito <lib.martinito@pm.me>
2024-03-10 16:18:31 +0800

    fix: update editorRefreshScreen function to clear screen and move cursor to top left position

--- kilo.c
+++ kilo.c
@@ -54,7 +54,10 @@ char editorReadKey() {
 
 /*** output ***/
 
-void editorRefreshScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
 
 /*** input ***/
 
